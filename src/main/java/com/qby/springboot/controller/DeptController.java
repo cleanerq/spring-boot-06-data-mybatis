fa527e666371bb6de77fa7139b4640ea6339b4e9
cleanerq <qbyun130@163.com>
2020-08-12 00:11:12 +0800

    import

--- src/main/java/com/qby/springboot/controller/DeptController.java
+++ src/main/java/com/qby/springboot/controller/DeptController.java
@@ -1,18 +1,24 @@
 package com.qby.springboot.controller;
 
 import com.qby.springboot.bean.Department;
+import com.qby.springboot.bean.Employee;
 import com.qby.springboot.mapper.DepartmentMapper;
+import com.qby.springboot.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import sun.rmi.server.InactiveGroupException;
 
 @Controller
 public class DeptController {
 
     @Autowired
     private DepartmentMapper departmentMapper;
+    @Autowired
+    private EmployeeMapper employeeMapper;
 
     @ResponseBody
     @GetMapping("/dept/{id}")
@@ -26,4 +32,11 @@ public class DeptController {
         departmentMapper.insertDept(department);
         return department;
     }
+
+    @ResponseBody
+    @RequestMapping("/emp/{id}")
+    public Employee getEmp(@PathVariable("id") Integer id) {
+        return employeeMapper.getEmpById(id);
+    }
+
 }
--- src/main/java/com/qby/springboot/mapper/EmployeeMapper.java
+++ src/main/java/com/qby/springboot/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.qby.springboot.mapper;
 
 import com.qby.springboot.bean.Employee;
+import org.springframework.stereotype.Repository;
 
 //@Mapper 或者 @MapperScan 将接口扫描装配到容器中
+@Repository
 public interface EmployeeMapper {
 
     public Employee getEmpById(Integer id);
